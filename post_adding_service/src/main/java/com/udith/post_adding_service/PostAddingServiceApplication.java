c822062dad57bc0e594a026485bc9a0329d9d9a4
Udith <udithshalinda2@gmail.com>
2019-12-27 19:46:31 +0530

    post adding service initial commit

---
+++ post_adding_service/src/main/java/com/udith/post_adding_service/PostAddingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.udith.post_adding_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostAddingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostAddingServiceApplication.class, args);
+	}
+
+}
---
+++ post_adding_service/src/test/java/com/udith/post_adding_service/PostAddingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udith.post_adding_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostAddingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

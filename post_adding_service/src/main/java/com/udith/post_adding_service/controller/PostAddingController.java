6ec1fcafa7bf86e3f351df3f95840f443026fa22
Udith <udithshalinda2@gmail.com>
2019-12-28 11:04:08 +0530

    discovery server added

--- post_adding_service/src/main/java/com/udith/post_adding_service/controller/PostAddingController.java
+++ post_adding_service/src/main/java/com/udith/post_adding_service/controller/PostAddingController.java
@@ -2,6 +2,12 @@ package com.udith.post_adding_service.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.udith.post_adding_service.model.Post;
+import com.udith.post_adding_service.repository.PostRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -11,11 +17,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/post")
 public class PostAddingController{
 
+    @Autowired
+    private PostRepository postRepository;
+
     @PostMapping("/add")
-    public String postMethodName(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
+    public String addPost(@RequestBody Post post) {
+        Post resPost = this.postRepository.save(post);
+        return resPost.getId().toString();
     }
+
+    @PostMapping("/addComment")
+    public String addComment(@RequestBody String comment ){
+        //need to complete;
+        return comment;
+    }
+
+    @PostMapping("/updatePost")
+    public String updatePost(){
+        return "sfsfs";
+    }
+
+    @PostMapping("/addLike/{userId}")
+    public String addLike(@PathVariable("userId")String userId){
+        return userId;
+    }
+
     
 }

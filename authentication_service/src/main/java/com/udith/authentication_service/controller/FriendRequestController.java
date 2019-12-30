c7d29ea8cd1c8862ca6eb7a633a484352296f035
Udith <udithshalinda2@gmail.com>
2019-12-30 22:00:39 +0530

    update

--- authentication_service/src/main/java/com/udith/authentication_service/controller/FriendRequestController.java
+++ authentication_service/src/main/java/com/udith/authentication_service/controller/FriendRequestController.java
@@ -3,20 +3,14 @@ package com.udith.authentication_service.controller;
 import com.udith.authentication_service.model.FriendRequest;
 import com.udith.authentication_service.model.User;
 import com.udith.authentication_service.repository.UserRepository;
-import com.udith.authentication_service.service.GraphQLService;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import graphql.ExecutionResult;
 
 @RestController
 @RequestMapping("/friendRequest")
@@ -24,8 +18,8 @@ public class FriendRequestController{
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    GraphQLService graphQLServie;
+    // @Autowired
+    // GraphQLService graphQLServie;
 
 
     @PutMapping("/send")
